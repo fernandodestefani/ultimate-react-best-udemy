eed7edd25e991e542872944d23350cccb1596ba8
Fernando Destefani <henriqueorei@gmail.com>
2025-10-05 13:14:28 -0300

    finalize Fast React Pizza app

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -117,15 +117,16 @@ function Menu() {
 // conditional rendering => ternary operation to return some piece of JSX based on a condition or multiple returns to return something entirely different
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
+
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
