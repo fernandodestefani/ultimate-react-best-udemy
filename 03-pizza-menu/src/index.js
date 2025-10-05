ba2d6b7dc79e5c1659369b6bf6bcbde4ca5f2a51
Fernando Destefani <henriqueorei@gmail.com>
2025-10-05 08:43:09 -0300

    destructure props

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -108,16 +108,16 @@ function Menu() {
 
 // conditional rendering => ternary operation to return some piece of JSX based on a condition or multiple returns to return something entirely different
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({pizzaObj}) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -142,7 +142,7 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen ? <Order closeHour={closeHour}/> : (
+      {isOpen ? <Order closeHour={closeHour} openHour={openHour}/> : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
@@ -152,10 +152,10 @@ function Footer() {
   // return React.createElement("footer", null, "We're currently open!")
 }
 
-function Order(props) {
+function Order({closeHour, openHour}) {
   return (
     <div className="order">
-      <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
+      <p>We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.</p>
       <button className="btn">Order</button>
     </div>
   );
