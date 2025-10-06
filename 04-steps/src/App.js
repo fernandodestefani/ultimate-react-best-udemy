1bbe9ca9009a5d6a4f0b3a46b3fbfad1fe279cd6
Fernando Destefani <henriqueorei@gmail.com>
2025-10-06 11:40:31 -0300

    implement step navigation with useState in Steps component

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,22 +7,24 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  // define the state variable using the state function
+  const [step, setStep] = useState(1); 
+
   // event handler functions
   function handlePrevious() {
-    
+    if (step > 1) setStep(step - 1)
   }
   
   function handleNext() {
-
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">Step {step}: {messages[step - 1]}</p>
       <div className="buttons">
