8ac304a54acd569ef89ff790a697a94254d19e76
Fernando Destefani <henriqueorei@gmail.com>
2025-10-08 20:36:42 -0300

    implement step-based counter with dynamic count

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,55 +1,43 @@
 import { useState } from "react";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
-
 export default function App() {
   // define the state variable using the state function
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const today = new Date().toDateString();
 
   // event handler functions
-  function handlePrevious() {
-    if (step > 1) setStep((s) => s - 1);
+  function handleStepDecrease() {
+    setStep((s) => s - 1);
   }
 
-  function handleNext() {
-    if (step < 3) setStep((s) => s + 1);
+  function handleStepIncrease() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountDecrease() {
+    setCount((c) => c - step)
+  } 
+
+  function handleCountIncrease() {
+    setCount((c) => c + step)
   }
 
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
-
-      {isOpen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-          </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-          <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
-    </>
+    <div className="container">
+      <div>
+        <button onClick={handleStepDecrease}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={handleStepIncrease}>+</button>
+      </div>
+      <div>
+        <button onClick={handleCountDecrease}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={handleCountIncrease}>+</button>
+      </div>
+      <p>Today is {today}</p>
+      {/* days from ; days ago was */}
+    </div>
   );
 }
