83395e870936a9fa6ebf32a6bdf4f034f83dd85e
Fernando Destefani <henriqueorei@gmail.com>
2025-10-17 08:55:42 -0300

    make a reusable step message using the 'children' prop in the steps application

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -33,9 +33,11 @@ export default function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>
+            {messages[step - 1]}
+          </StepMessage>
+
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈🏼</span> Previous
@@ -47,6 +49,15 @@ export default function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({step, children}) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
