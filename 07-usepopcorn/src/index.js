daae44968c518af08b36e304d32241ea65f38f1b
Fernando Destefani <henriqueorei@gmail.com>
2025-11-14 11:27:03 -0300

    improve reusability with props as a component API

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
+    <StarRating color='blue' size={24} className='test' defaultRating={3}/>
   </React.StrictMode>
 );
