e05ad070ded31c057e9d7a700833204773813e47
Fernando Destefani <henriqueorei@gmail.com>
2025-11-26 20:20:02 -0300

    create useLocalStorageState

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -14,10 +15,7 @@ export default function App() {
 
   const { movies, isLoading, error } = useMovies(query, handleCloseMovie);
 
-  const [watched, setWatched] = useState(function() {
-    const storedValue = localStorage.getItem('watched');
-    return JSON.parse(storedValue);
-  })
+  const [watched, setWatched] = useLocalStorageState([], "watched");
   
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -34,10 +32,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(function() {
-    localStorage.setItem("watched", JSON.stringify(watched))
-  }, [watched])
 
   return (
     <>
