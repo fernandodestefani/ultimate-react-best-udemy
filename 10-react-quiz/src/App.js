05e01ec8bd109d5d5f340698227441bc51e32b0e
Fernando Destefani <henriqueorei@gmail.com>
2025-11-30 16:07:50 -0300

    display question and options

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,16 +1,17 @@
 import { useEffect, useReducer } from "react";
-import Header from "./Header.js";
-import Main from "./Main.js";
-import Loader from "./Loader.js";
-import Error from "./Error.js";
-import StartScreen from "./StartScreen.js";
-import Question from "./Question.js";
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import Loader from "./components/Loader.js";
+import Error from "./components/Error.js";
+import StartScreen from "./components/StartScreen.js";
+import Question from "./components/Question.js";
 
 const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -26,7 +27,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+  const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -45,7 +46,7 @@ export default function App() {
         {status === "loading" && <Loader/>}
         {status === 'error' && <Error />}
         {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-        {status === 'active' && <Question />}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   );
