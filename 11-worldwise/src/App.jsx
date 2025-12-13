f35696e3c725a9c1631fc5c7d7f324d2d8f84592
Fernando Destefani <henriqueorei@gmail.com>
2025-12-13 18:18:04 -0300

    use programmatic navigation to access the form component in the UI

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from "./pages/AppLayout";
 import City from "./components/City"
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -51,7 +52,7 @@ function App() {
           />
           <Route path="cities/:id" element={<City /> }/>
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form/>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
