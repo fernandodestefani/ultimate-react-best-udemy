40ad58374368e836380c024e0c55cdc6afea0e24
Fernando Destefani <henriqueorei@gmail.com>
2026-01-13 19:55:16 -0300

    make an api call to frankfurter with redux thunks

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,12 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/costumers/customerSlice";
+
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
