529db9686c0ae129ac712060feb2296e96ab11d5
Fernando Destefani <henriqueorei@gmail.com>
2026-01-19 20:43:33 -0300

    style font family

--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -8,7 +8,7 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
   
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+    <div className="grid grid-rows-[auto_1fr_auto] h-dvh">
       {isLoading && <Loader/>}
       <Header/>
 
