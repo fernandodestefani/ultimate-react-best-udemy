482cbb7252fb6e1aa96c9a608a6007bbe6773290
Fernando Destefani <henriqueorei@gmail.com>
2026-01-20 20:35:25 -0300

    style the cart

--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -6,7 +6,8 @@ function Button({ children, disabled, to, type }) {
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
-    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs"
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary: "inline-block cursor-pointer rounded-full border-2 border-stone-300 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5" 
   };
 
   if (to) {
