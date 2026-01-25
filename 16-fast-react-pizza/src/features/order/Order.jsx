59f990c8969ad5120349e7690a31518092351748
Fernando Destefani <henriqueorei@gmail.com>
2026-01-25 09:53:42 -0300

    fetch ingredients data from menu without navigation to show them in the order page using usefetcher

--- 16-fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -8,9 +8,17 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(function(){
+    if(!fetcher.data && fetcher.state === "idle") fetcher.load("/menu")
+  }, [fetcher])
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -43,7 +51,7 @@ function Order() {
         <p className="text-xs text-stone-500">(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
 
-      <ul className="divide-y divide-stone-200 border-b border-t border-stone-200">{cart.map(item => <OrderItem item={item} key={item.pizzaId} />)}</ul>
+      <ul className="divide-y divide-stone-200 border-b border-t border-stone-200">{cart.map(item => <OrderItem item={item} key={item.pizzaId} ingredients={fetcher.data?.find(el => el.id === item.pizzaId).ingredients} isLoadingIngredients={fetcher.state === "loading"} />)}</ul>
 
       <div className="px-6 py-5 bg-stone-200 space-y-2">
         <p className="text-sm font-medium text-stone-600">Price pizza: {formatCurrency(orderPrice)}</p>
