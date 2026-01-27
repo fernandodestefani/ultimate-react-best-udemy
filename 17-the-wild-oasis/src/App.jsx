d0127f808f2c72b0dcbe4554f5d7fbfc86f16075
Fernando Destefani <henriqueorei@gmail.com>
2026-01-27 20:41:27 -0300

    add global styles using styled components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,9 +1,26 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
+
 function App() {
   return (
-    <div>
-      Hello, World!
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 17-the-wild-oasis/src/ui/MainNav.jsx
+++ 17-the-wild-oasis/src/ui/MainNav.jsx
@@ -1,12 +1,4 @@
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import {
-  HiOutlineCalendarDays,
-  HiOutlineCog6Tooth,
-  HiOutlineHome,
-  HiOutlineHomeModern,
-  HiOutlineUsers,
-} from "react-icons/hi2";
 
 const NavList = styled.ul`
   display: flex;
@@ -14,7 +6,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const StyledNavLink = styled(NavLink)`
+const Link = styled.a`
   &:link,
   &:visited {
     display: flex;
@@ -52,44 +44,3 @@ const StyledNavLink = styled(NavLink)`
     color: var(--color-brand-600);
   }
 `;
-
-function MainNav() {
-  return (
-    <nav>
-      <NavList>
-        <li>
-          <StyledNavLink to="/dashboard">
-            <HiOutlineHome />
-            <span>Home</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/bookings">
-            <HiOutlineCalendarDays />
-            <span>Bookings</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/cabins">
-            <HiOutlineHomeModern />
-            <span>Cabins</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/users">
-            <HiOutlineUsers />
-            <span>Users</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/settings">
-            <HiOutlineCog6Tooth />
-            <span>Settings</span>
-          </StyledNavLink>
-        </li>
-      </NavList>
-    </nav>
-  );
-}
-
-export default MainNav;
