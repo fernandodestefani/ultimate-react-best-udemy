0fc7aa4f1898b1fbdf9bd283cb5863071e37c8f8
Fernando Destefani <henriqueorei@gmail.com>
2026-02-04 20:25:42 -0300

    display toast notifications

--- 17-the-wild-oasis/src/ui/AppLayout.jsx
+++ 17-the-wild-oasis/src/ui/AppLayout.jsx
@@ -3,16 +3,24 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
-const Main = styled.main`
-  background-color: var(--color-grey-0);
-  padding: 4rem 4.8rem 6.4rem;
-`;
-
 const StyledAppLayout = styled.div`
   display: grid;
   grid-template-columns: 26rem 1fr;
   grid-template-rows: auto 1fr;
   height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-0);
+  padding: 4rem 4.8rem 6.4rem;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
@@ -21,7 +29,9 @@ function AppLayout() {
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
