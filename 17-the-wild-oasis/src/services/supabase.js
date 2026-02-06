42de88079d5939b1591ecb37dad722ae08fc803e
Fernando Destefani <henriqueorei@gmail.com>
2026-02-06 16:56:32 -0300

    upload image to supabase

--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://asobsdnwbuibkcjbtmiq.supabase.co";
+export const supabaseUrl = "https://asobsdnwbuibkcjbtmiq.supabase.co";
 const supabaseKey = "sb_publishable_8reQx6fOooSaZg60kNqRmQ_SAEXNxVH";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
