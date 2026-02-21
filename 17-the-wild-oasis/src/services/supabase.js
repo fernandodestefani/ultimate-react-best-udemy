a5032962f3ead0fe9c4e67ea74bc7a9e23d7a656
Fernando Destefani <henriqueorei@gmail.com>
2026-02-21 15:54:46 -0300

    build the app header include the user's fullName and avatar

--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://asobsdnwbuibkcjbtmiq.supabase.co";
-const supabaseKey = "sb_publishable_8reQx6fOooSaZg60kNqRmQ_SAEXNxVH";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
