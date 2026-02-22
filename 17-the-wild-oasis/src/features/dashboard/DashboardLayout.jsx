1d96df06df988b3c617d122d4bbcbe6133c1606d
Fernando Destefani <henriqueorei@gmail.com>
2026-02-22 16:03:12 -0300

    build the dashboard layout

--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,13 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
