b7c204f113f67da87c6ec1dac35e3ab36893b265
Fernando Destefani <henriqueorei@gmail.com>
2026-03-17 20:21:22 -0300

    organizate project folders and main files

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,3 +1,10 @@
+import Counter from "../_components/Counter";
+/* alias "@/app/_components/Counter" */
+
 export default function Page() {
-  return <h1>Cabins page</h1>
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
